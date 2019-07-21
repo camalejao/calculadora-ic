850ab165770c041cb7a65d4ce71ca9147edfd428
Rodrigo Santos da Silva <rodrigosnt3@gmail.com>
2019-07-20 23:48:43 -0300

    Iniciando as telas da aplicação, desenvolvendo o footer, e organizando os diretórios

---
+++ js/chamaRedesSociais.js
@@ -0,0 +1,14 @@
+var github = document.querySelector(".github");
+var linkedin = document.querySelector(".linkedin");
+
+github.addEventListener("click", function(event){
+  window.open('https://github.com/Raksantos');
+  window.open('https://github.com/camalejao/');
+  window.open('https://github.com/clemensss');
+});
+
+linkedin.addEventListener("click", function(event){
+  window.open('https://www.linkedin.com/in/rodrigo-santos-da-silva-175538175/');
+  window.open('https://www.linkedin.com/in/jo%C3%A3o-victor-falc%C3%A3o-360862188/');
+  window.open('https://www.linkedin.com/in/clemens-schrage-79509018a/ ')
+});
